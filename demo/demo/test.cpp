7769abbf88db5205738d2ac2f20a428b846bbc3a
ranjun <1600848783@qq.com>
2025-01-10 17:44:50 +0800

    explict 测试

--- demo/demo/test.cpp
+++ demo/demo/test.cpp
@@ -1,7 +1,31 @@
 #include<iostream>
 using namespace std;
 
+class MyClass1 {
+public:
+	MyClass1(int value) {
+
+	}
+
+	int sum(int a, int b) {
+		return a + b;
+	}
+};
+
+class MyClass2 {
+public:
+	explicit MyClass2(int value) {
+
+	}
+
+	int dec(int a, int b) {
+		return a - b;
+	}
+};
+
 int main() {
-	cout << "hello world" << endl;
-	return 0;
+	MyClass1 obj = 10;
+	MyClass2 obj1(11);
+
+	obj.sum(10, 10);
 }
