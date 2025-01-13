e788af474779bbee129d702c3b9d35eac4c97cbf
ranjun <1600848783@qq.com>
2025-01-13 15:51:58 +0800

    动态更新demo

--- demo/demo/test.cpp
+++ demo/demo/test.cpp
@@ -1,31 +1,76 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <chrono>
+#include <thread>
+#include <unordered_map>
+#include <string>
 
-class MyClass1 {
+namespace fs = std::filesystem;
+
+// 配置类
+class Config {
 public:
-	MyClass1(int value) {
-
+	Config(const std::string& filename) : filename_(filename) {
+		loadConfig();
 	}
 
-	int sum(int a, int b) {
-		return a + b;
-	}
-};
+	void loadConfig() {
+		std::ifstream file(filename_);
+		if (!file.is_open()) {
+			std::cerr << "无法打开配置文件: " << filename_ << std::endl;
+			return;
+		}
 
-class MyClass2 {
-public:
-	explicit MyClass2(int value) {
-
+		std::string line;
+		while (std::getline(file, line)) {
+			size_t pos = line.find('=');
+			if (pos != std::string::npos) {
+				std::string key = line.substr(0, pos);
+				std::string value = line.substr(pos + 1);
+				config_[key] = value;
+			}
+		}
+		file.close();
+		last_modified_ = fs::last_write_time(filename_);
 	}
 
-	int dec(int a, int b) {
-		return a - b;
+	std::string get(const std::string& key) const {
+		auto it = config_.find(key);
+		if (it != config_.end()) {
+			return it->second;
+		}
+		return "";
 	}
+
+	bool checkAndReload() {
+		auto current_modified = fs::last_write_time(filename_);
+		if (current_modified != last_modified_) {
+			loadConfig();
+			last_modified_ = current_modified;
+			return true;
+		}
+		return false;
+	}
+
+private:
+	std::string filename_;
+	std::unordered_map<std::string, std::string> config_;
+	fs::file_time_type last_modified_;
 };
 
 int main() {
-	MyClass1 obj = 10;
-	MyClass2 obj1(11);
+	Config config("config.txt");
 
-	obj.sum(10, 10);
+	while (true) {
+		if (config.checkAndReload()) {
+			std::cout << "配置文件已更新：" << std::endl;
+			std::cout << "interval: " << config.get("interval") << std::endl;
+			std::cout << "enabled: " << config.get("enabled") << std::endl;
+		}
+
+		std::this_thread::sleep_for(std::chrono::seconds(5)); // 每5秒检查一次
+	}
+
+	return 0;
 }
